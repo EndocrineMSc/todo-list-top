843866d8af554159f059b66fc8b072bdce2874d2
EndocrineMSc <bamsc.studio@gmail.com>
2024-01-18 14:42:41 +0100

    Inital set-up including webpack and basic folder- and file-structure

---
+++ src/eventHandler.js
@@ -0,0 +1,25 @@
+//events (publish subscribe) pattern
+const eventHandler = {
+    events: {},
+    subscribe: function (eventName, fn) {
+      this.events[eventName] = this.events[eventName] || [];
+      this.events[eventName].push(fn);
+    },
+    remove: function(eventName, fn) {
+      if (this.events[eventName]) {
+        for (var i = 0; i < this.events[eventName].length; i++) {
+          if (this.events[eventName][i] === fn) {
+            this.events[eventName].splice(i, 1);
+            break;
+          }
+        };
+      }
+    },
+    invoke: function (eventName, data) {
+      if (this.events[eventName]) {
+        this.events[eventName].forEach(function(fn) {
+          fn(data);
+        });
+      }
+    }
+};
